3f861807f528e6f9c22407050a9165410b4c0153
m1k3s <pbrane0@gmail.com>
2017-03-28 15:31:38 -0400

    updated to GPLv3

--- reptiles/common/EntityChameleon.java
+++ reptiles/common/EntityChameleon.java
@@ -1,21 +1,41 @@
-//  
+// EntityChameleon.java
+// Copyright (c) 2017 Michael Sheppard
+//
 //  =====GPL=============================================================
-//  This program is free software; you can redistribute it and/or modify
+//  reptilemod is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; version 2 dated June, 1991.
-// 
-//  This program is distributed in the hope that it will be useful, 
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU General Public License
-//  along with this program;  if not, write to the Free Software
-//  Foundation, Inc., 675 Mass Ave., Cambridge, MA 02139, USA.
+//  along with this program.  If not, see http://www.gnu.org/licenses.
 //  =====================================================================
 //
+
+// EntityChameleon.java
+// Copyright (c) 2017 Michael Sheppard
 //
+//  =====GPL=============================================================
+//  reptilemod is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see http://www.gnu.org/licenses.
+//  =====================================================================
+//
+
 package com.reptiles.common;
 
 import net.minecraft.entity.EntityAgeable;
